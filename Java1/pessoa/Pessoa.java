3f9bd44e1d9ee838840e137fc4e1ab5dfde3a3c2
JuanFila <juanfilapablo@hotmail.com>
2024-02-17 23:03:08 -0300

    add herança

--- Java1/pessoa/Pessoa.java
+++ Java1/pessoa/Pessoa.java
@@ -1,12 +1,32 @@
 package pessoa;
 
 public class Pessoa {
-  String nome; //atributo
-  int idade;
-  String cpf;
+  private String nome; // atributo
+  private int idade;
+  private String cpf;
 
-  String imprimirDadosDaPessoa(){
-    return "Nome: "  +  nome + " idade: " + idade +
-    " cpf: " + cpf;
-  } //metodo
+  public String getCpf() {
+    return cpf;
+  }
+  public int getIdade() {
+    return idade;
+  }
+  public String getNome() {
+    return nome;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  String imprimirDadosDaPessoa() {
+    return "Nome: " + nome + " idade: " + idade +
+        " cpf: " + cpf;
+  } // metodo
 }
--- Java1/pessoa/TesteDePessoa.java
+++ Java1/pessoa/TesteDePessoa.java
@@ -2,12 +2,22 @@ package pessoa;
 
 public class TesteDePessoa {
   public static void main(String[] args) {
-    Pessoa pessoa = new Pessoa();
-    pessoa.cpf = "000-000-000-00";
-    pessoa.idade = 22;
-    pessoa.nome = "JonDoe";
-  
-    System.out.println(pessoa.imprimirDadosDaPessoa());
+    Professor professor = new Professor();
+    professor.setCpf("000-000-000-00");
+    professor.setIdade(22);
+    professor.setNome("JonDoe");
+    professor.setSalario(20000);
+
+    System.out.println(professor.imprimirDadosDaPessoa());
+
+
+    Aluno aluno = new Aluno();
+    aluno.setCpf("000-000-000-00");
+    aluno.setIdade(22);
+    aluno.setNome("JonDoe");
+    aluno.setMatricula("202020");
+
+    System.out.println(aluno.imprimirDadosDaPessoa());
   }
-  
+
 }
