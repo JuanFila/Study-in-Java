5bf66edce7db27a066b73e2620f2ebcdc9c93cd2
JuanFila <juanfilapablo@hotmail.com>
2024-02-20 21:03:26 -0300

    add springboot

---
+++ Java1/wrapper/Wrapper.java
@@ -0,0 +1,29 @@
+package wrapper;
+
+public class Wrapper {
+  public static void main(String[] args) {
+    int tipoInt = 1;
+    float tipoFloat = 1;
+    boolean tipoBoolean = true;
+
+    // Para usar um wrapper class inciamos com a LETRA maiuscula menos para int e
+    // char
+    /*
+    8 tipos primitivos:
+     * byte;
+     * short;
+     * int;
+     * long;
+     * boolean;
+     * char;
+     * float;
+     * double.
+     */
+    // tipos primitivos não aceitam valor como null
+
+    Float tipFloat2 = 1F; // Dfinir que é um float com F após numero
+    Boolean tiBoolean2 = false;
+    Integer tipoInteger = 1; // int
+
+  }
+}
---
+++ SpringBoot/first-project-springboot/src/test/java/br/com/example/firstprojectspringboot/FirstProjectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.firstprojectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
