c998d8eb11778e64b029640754480f7ff8f099ab
JuanFila <juanfilapablo@hotmail.com>
2024-02-28 00:05:28 -0300

    add project

--- SpringBoot/first-project-springboot/src/main/java/br/com/example/ioc_di/IocController.java
+++ SpringBoot/first-project-springboot/src/main/java/br/com/example/ioc_di/IocController.java
@@ -2,6 +2,8 @@ package br.com.example.ioc_di;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -19,5 +21,6 @@ public class IocController {
       return resultado;
 
   }
+  
   
 }
---
+++ gestao_vagas/src/main/java/br/project/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,26 @@
+package br.project.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class CandidateEntity {
+  
+  private UUID id;
+  private String name;
+  @NotBlank()
+  @Pattern(regexp = "\\S+", message = "Remova os espaços")
+  private String  userName;
+  @Email(message = "O campo(email) deve conter um e-mail válido")
+  private String email;
+  @Length(min = 10, max = 100, message="A senha precisa ter no mínimo (10) caracteres")
+  private String password;
+  private String description;
+  private String curriculum;
+}
