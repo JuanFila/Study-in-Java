66f2f69e57f04b9bcf72b197c092f514224e2107
JuanFila <juanfilapablo@hotmail.com>
2024-03-04 22:03:55 -0300

    case

--- gestao_vagas/src/main/java/br/project/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestao_vagas/src/main/java/br/project/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -3,12 +3,12 @@ package br.project.gestao_vagas.modules.candidate.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.project.gestao_vagas.exceptions.UserFoundException;
 import br.project.gestao_vagas.modules.candidate.CandidateEntity;
-import br.project.gestao_vagas.modules.candidate.CandidateRepository;
+import br.project.gestao_vagas.modules.candidate.useCase.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,15 +17,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CandidateController   {
 
     @Autowired
-    private CandidateRepository candidateRepository;
+    private CreateCandidateUseCase createCandidateUseCase;
 
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        this.candidateRepository.
-        findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
-        .ifPresent((user) -> {
-            throw new UserFoundException();
-        });
-        return this.candidateRepository.save(candidateEntity);
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            var resut = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(resut);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
